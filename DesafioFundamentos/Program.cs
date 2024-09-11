d956e4dbacbdc4e6c44843af92d86ef1b9e5a7f4
wilianmorimoto <wilianhmorimoto@gmail.com>
2024-09-11 09:12:42 -0300

    desafio concluido

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,8 +6,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" + "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
